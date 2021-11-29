80aee72663c837857de1e880138201fdabc1c579
danha138 <danha138@gmail.com>
2021-11-29 08:57:43 -0500

    Created BlockerModel for app blocker/lockout Added new clipart for lockout

--- app/src/main/java/com/example/a11_9project/ui/lockout/LockoutFragment.java
+++ app/src/main/java/com/example/a11_9project/ui/lockout/LockoutFragment.java
@@ -4,42 +4,34 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.a11_9project.R;
 import com.example.a11_9project.databinding.FragmentLockoutBinding;
+import com.shuhart.stepview.StepView;
 
-public class LockoutFragment extends Fragment {
+public class LockoutFragment extends AppCompatActivity {
 
-    private LockoutViewModel lockoutViewModel;
-    private FragmentLockoutBinding binding;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        lockoutViewModel =
-                new ViewModelProvider(this).get(LockoutViewModel.class);
-
-        binding = FragmentLockoutBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textLockout;
-        lockoutViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
-    }
+    StepView stepView;
+    LinearLayout normalLayout;
+    TextView status_password;
+    RelativeLayout relativeLayout;
+    //13:06
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_lockout);
+
+
     }
 }
---
+++ app/src/main/java/com/example/a11_9project/ui/lockout/Model/BlockerModel.java
@@ -0,0 +1,7 @@
+package com.example.a11_9project.ui.lockout.Model;
+
+public class BlockerModel {
+
+    private String PASSWORD_KEY = "PASSWORD_KEY";
+
+}

2792c92668e84ef2cec2ebe9450c1d7c78d3704e
danha138 <danha138@gmail.com>
2021-11-30 19:47:59 -0500

    Added temporary functionality to the following files

--- app/src/main/java/com/example/a11_9project/ui/lockout/LockoutFragment.java
+++ app/src/main/java/com/example/a11_9project/ui/lockout/LockoutFragment.java
@@ -1,5 +1,7 @@
 package com.example.a11_9project.ui.lockout;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,13 +13,21 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.andrognito.patternlockview.PatternLockView;
+import com.andrognito.patternlockview.listener.PatternLockViewListener;
+import com.andrognito.patternlockview.utils.PatternLockUtils;
 import com.example.a11_9project.R;
 import com.example.a11_9project.databinding.FragmentLockoutBinding;
+import com.example.a11_9project.ui.lockout.Model.BlockerModel;
 import com.shuhart.stepview.StepView;
+
+import java.util.List;
+import java.util.ResourceBundle;
 
 public class LockoutFragment extends AppCompatActivity {
 
@@ -25,13 +35,125 @@ public class LockoutFragment extends AppCompatActivity {
     LinearLayout normalLayout;
     TextView status_password;
     RelativeLayout relativeLayout;
-    //13:06
+
+    BlockerModel utilsBlock;
+    String userPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_lockout);
 
+        initLayout();
 
+        initPatternListener();
+    }
+
+    private void initPatternListener() {
+        PatternLockView patternLockView = findViewById(R.id.pattern_view);
+        patternLockView.addPatternLockListener(new PatternLockViewListener() {
+            @Override
+            public void onStarted() {
+
+            }
+
+            @Override
+            public void onProgress(List<PatternLockView.Dot> progressPattern) {
+
+            }
+
+            @Override
+            public void onComplete(List<PatternLockView.Dot> pattern) {
+
+                String password = PatternLockUtils.patternToString(patternLockView, pattern);
+
+                if(password.length() < 4){
+                    status_password.setText(utilsBlock.SHEMA_FAILED);
+                    patternLockView.clearPattern();
+                    return;
+                }
+
+                if(utilsBlock.getPassword() == null){
+                    if(utilsBlock.isFirstStep()){
+                        userPassword = password;
+                        utilsBlock.setFirstStep(false);
+                        status_password.setText(utilsBlock.STATUS_NEXT_STEP);
+                        stepView.go(1, true);
+                    }
+                    else {
+                        if(userPassword.equals(password)){
+                            utilsBlock.setPassword(userPassword);
+                            status_password.setText(utilsBlock.STATUS_PASSWORD_CORRECT);
+                            stepView.done(true);
+
+                            startAct();
+                        }
+                        else{
+                            status_password.setText(utilsBlock.STATUS_PASSWORD_INCORRECT);
+                        }
+                    }
+                }
+                else{
+                    if(utilsBlock.isCorrect(password)){
+                        status_password.setText(utilsBlock.STATUS_PASSWORD_CORRECT);
+                        startAct();
+                    }
+                    else{
+                        status_password.setText(utilsBlock.STATUS_PASSWORD_INCORRECT);
+                    }
+                }
+                patternLockView.clearPattern();
+            }
+
+            @Override
+            public void onCleared() {
+
+            }
+        });
+    }
+
+    private void startAct() {
+
+        startActivity(new Intent(this, LockoutFragment.class));
+        finish();
+
+    }
+
+    private void initLayout() {
+
+        stepView = findViewById(R.id.step_view);
+        normalLayout = findViewById(R.id.normal_layout);
+        relativeLayout = findViewById(R.id.root);
+        status_password = findViewById(R.id.status_password);
+        utilsBlock = new BlockerModel(this);
+        status_password.setText(utilsBlock.STATUS_FIRST_STEP);
+
+        if(utilsBlock.getPassword() == null){
+            normalLayout.setVisibility(View.GONE);
+            stepView.setVisibility(View.VISIBLE);
+            stepView.setStepsNumber(2);
+            stepView.go(0, true);
+        }
+        else{
+            normalLayout.setVisibility(View.VISIBLE);
+            stepView.setVisibility(View.GONE);
+
+            int backColor = ResourcesCompat.getColor(getResources(), R.color.black, null);
+            relativeLayout.setBackgroundColor(backColor);
+            status_password.setTextColor(Color.WHITE);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(utilsBlock.getPassword() == null && !utilsBlock.isFirstStep()){
+            stepView.go(0, true);
+            utilsBlock.setFirstStep(true);
+            status_password.setText(utilsBlock.STATUS_FIRST_STEP);
+        }
+        else{
+            finish();
+            super.onBackPressed();
+        }
     }
 }
--- app/src/main/java/com/example/a11_9project/ui/lockout/Model/BlockerModel.java
+++ app/src/main/java/com/example/a11_9project/ui/lockout/Model/BlockerModel.java
@@ -1,7 +1,42 @@
 package com.example.a11_9project.ui.lockout.Model;
 
+import android.content.Context;
+
+import io.paperdb.Paper;
+
 public class BlockerModel {
 
     private String PASSWORD_KEY = "PASSWORD_KEY";
+    public String STATUS_FIRST_STEP = "Draw an unlock pattern";
+    public String STATUS_NEXT_STEP = "Draw a pattern again to confirm";
+    public String STATUS_PASSWORD_CORRECT = "Pattern Set";
+    public String STATUS_PASSWORD_INCORRECT = "Try again";
+    public String SHEMA_FAILED = "Connect at least 4 dots";
+
+    private boolean isFirstStep = true;
+
+    public BlockerModel(Context context){
+        Paper.init(context);
+    }
+
+    public void setPassword(String password){
+        Paper.book().write(PASSWORD_KEY, password);
+    }
+
+    public String getPassword(){
+        return Paper.book().read(PASSWORD_KEY);
+    }
+
+    public boolean isFirstStep(){
+        return isFirstStep;
+    }
+
+    public void setFirstStep(boolean firstStep){
+        isFirstStep = firstStep;
+    }
+
+    public boolean isCorrect(String password){
+        return password.equals(getPassword());
+    }
 
 }
